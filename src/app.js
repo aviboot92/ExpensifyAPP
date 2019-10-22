9681a0c795114ca8b30eb67b9b081d9c9880c562
aviboot92 <avkarra@gmail.com>
2019-10-22 12:28:09 +0530

    Router-Dom-Added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from './components/IndecissionApp';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';          
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+
+const ExpenseDashBoardPage = () =>(
+    <div>
+        <p>I am Dash Board page</p>
+    </div>
+);
+
+const AddExpensePage = () =>(
+    <div>
+        <p>I am Add Expense page</p>
+    </div>
+);
+
+const EditExpensePage = () =>(
+    <div>
+        <p>I am Edit Expense Page</p>
+    </div>
+);
+
+const HelpPage = () =>(
+    <div>
+        <p>I am Help Page</p>
+    </div>
+);
+
+const NotFoundPage = () =>(
+    <div>
+        <p>!!!! 404 !!!!</p>
+        <Link to="/">Go Home</Link>
+    </div>
+); 
+
+const Header = () =>(
+    <header>
+        <h1>Expensify APP</h1>
+        <Link to='./'>Home</Link>
+        <Link to='./crete'>Add Expense</Link>
+        <Link to='./edit'>Edit Expense</Link>
+        <Link to='./help'>Help</Link>
+    </header>
+)
+
+const routes = (
+    <Router>
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path='./'>
+                        <ExpenseDashBoardPage />
+                </Route>
+                <Route path="./create" component={AddExpensePage}/>
+                <Route path="./edit" component={EditExpensePage}/>
+                <Route path="./help" component={HelpPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
+        </div>
+    </Router>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
