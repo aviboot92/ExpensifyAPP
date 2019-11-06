3e67994e25847b5af55be9c3d3a3d097559fc2e3
aviboot92 <avkarra@gmail.com>
2019-11-06 18:37:37 +0530

    Expenses action test cases were written and executed

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,1 +1,51 @@
 import {addExpense, removeExpense, editExpense} from '../../actions/expenses';
+
+test('should give the remove action object', () => {
+    const action = removeExpense({id: '123abc'});
+    expect(action).toEqual({type: 'REMOVE_EXPENSE', id: '123abc'});
+})
+
+test('should give edit action object', () => {
+    const action = editExpense("1234abc", {
+        descreption: 'April fool',
+        note: 'Dont fool',
+        amount: '1234'
+    });
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '1234abc',
+        updates: {
+            descreption: 'April fool',
+            note: 'Dont fool',
+            amount: '1234'
+        }
+    });
+})
+
+test('should give new object with some data and id', () => {
+    const action = addExpense({descreption: 'April fool', note: 'Dont fool', amount: '1234', createdAt: 1000});
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: expect.any(String),
+            descreption: 'April fool',
+            note: 'Dont fool',
+            amount: '1234',
+            createdAt: 1000
+        }
+    });
+})
+
+test('should give new object with default data', () => {
+    const action = addExpense();
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            descreption : '',
+            note : '',
+            amount : 0,
+            createdAt : 0,
+            id: expect.any(String)
+        }
+    })
+})
