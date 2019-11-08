b1d66a5a63404adde85b0be13c966ebc610ac3ec
aviboot92 <avkarra@gmail.com>
2019-11-08 10:31:17 +0530

    selectors test is failed

---
+++ src/tests/selectors/exepnses.test.js
@@ -0,0 +1,37 @@
+import getVisibleExpenses from '../../selectors/expenses';
+import expenses from '../../selectors/expenses';
+
+const exepnses = [
+    {
+        id: "1",
+        descreption: "Gum",
+        note: "",
+        amount: 195,
+        createdAt: 0
+    }, {
+        id: "2",
+        descreption: "Rent",
+        note: "",
+        amount: 19500000,
+        createdAt: -1000
+    }, {
+        id: "3",
+        descreption: "Credit Card",
+        note: "",
+        amount: 4500,
+        createdAt: 1000
+    }
+];
+
+test('should filter by text value',() => {
+    const filter ={
+        text: 'e',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+    }
+
+    const result = getVisibleExpenses(expenses, filter);
+    expect(result).toEqual([expenses[2], expenses[1]]);
+})
+
