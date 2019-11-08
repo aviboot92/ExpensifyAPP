a951b7c459307a632db4844b124318b1bdef3b0a
aviboot92 <avkarra@gmail.com>
2019-11-08 10:06:33 +0530

    test case for actions completed successfully

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,16 +1,20 @@
-const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
-    return expenses.filter((expense) => {
-        const textMatch = text === '' || expense.descreption.toLowerCase().includes(text.toLowerCase());
-        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= startDate;
-        return textMatch && startDateMatch && endDateMatch;
-    }).sort((a,b)=>{
-        if(sortBy === 'date'){
-            return a.createdAt < b.createdAt ? 1 : -1;
-        } else if(sortBy === 'amount'){
-            return a.amount < b.amount ? 1 : -1;
-        }
-    })
-}
+import moment from 'moment';
 
-export default getVisibleExpenses;
+// Get visible expenses
+
+export default (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses.filter((expense) => {
+    const createdAtMoment = moment(expense.createdAt);
+    const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+    const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1;
+    }
+  });
+};
