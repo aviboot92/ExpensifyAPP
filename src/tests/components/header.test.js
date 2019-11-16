62172efce370f7ac7a27b08986e87ae441f1fc10
aviboot92 <avkarra@gmail.com>
2019-11-16 12:08:53 +0530

    snapshots are integrated

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 // import ReactShallowRenderer from 'react-test-renderer/shallow';
+import toJSON from 'enzyme-to-json';
 import {shallow} from 'enzyme';
 import Header from '../../components/Header';
+import expectExport from 'expect';
 
 test('should render header correctly', () => {
+    const wrapper = shallow(<Header />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
-    const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').text()).toBe('Expensify APP');
+    // expect(wrapper.find('h1').text()).toBe('Expensify APP');
 })
